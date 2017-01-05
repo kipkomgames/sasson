2e826dc82d626b1ea8cf64ed9e8b59a533313000
kipkom <GuyShimoni@Guys-MacBook-Pro.local>
2017-01-05 16:17:40 +0200

    sasson
    sounds

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var path = require("path")
+var app = express()
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+})
